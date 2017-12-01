1a1518edbc39a06503c4bcb381e6d77d3c6a15e5
Maxence COLMANT <maxence.colmant@ynov.com>
2017-12-01 14:44:18 +0100

    architech

---
+++ inc/header.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
+    <meta content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=no" name="viewport" />
+    <meta content="Semantic-UI-Forest, collection of design, themes and templates for Semantic-UI." name="description" />
+    <meta content="Semantic-UI, Theme, Design, Template" name="keywords" />
+    <meta content="PPType" name="author" />
+    <meta content="#ffffff" name="theme-color" />
+    <title>Semantic-UI-Forest, collection of design, themes and templates for Semantic-UI.</title>
+    <link href="static/dist/semantic-ui/semantic.min.css" rel="stylesheet" type="text/css" />
+    <link href="static/stylesheets/default.css" rel="stylesheet" type="text/css" />
+    <link href="static/stylesheets/pandoc-code-highlight.css" rel="stylesheet" type="text/css" />
+    <script src="static/dist/jquery/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="ui large top fixed hidden menu">
+      <div class="ui container">
+        <a class="active item">Home</a><a class="item">Work</a><a class="item">Company</a><a class="item">Careers</a>
+        <div class="right menu">
+          <div class="item">
+            <a class="ui button">Log in</a>
+          </div>
+          <div class="item">
+            <a class="ui primary button">Sign Up</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--Sidebar Menu-->
+    <div class="ui vertical inverted sidebar menu">
+      <a class="active item">Home</a><a class="item">Work</a><a class="item">Company</a><a class="item">Careers</a><a class="item">Login</a><a class="item">Signup</a>
+    </div>
---
+++ index.php
@@ -0,0 +1,156 @@
+<?php include 'inc/header.php' ?>
+
+
+
+
+
+
+<!--Page Contents-->
+<div class="pusher">
+    <div class="ui inverted vertical masthead center aligned segment">
+        <div class="ui container">
+            <div class="ui large secondary inverted pointing menu">
+                <a class="toc item"><i class="sidebar icon"></i></a><a class="active item">Home</a><a class="item">Work</a><a class="item">Company</a><a class="item">Careers</a>
+                <div class="right item">
+                    <a class="ui inverted button">Log in</a><a class="ui inverted button">Sign Up</a>
+                </div>
+            </div>
+        </div>
+        <div class="ui text container">
+            <h1 class="ui inverted header">
+                Imagine-a-Company
+            </h1>
+            <h2>
+                Do whatever you want when you want to.
+            </h2>
+            <div class="ui huge primary button">
+                Get Started<i class="right arrow icon"></i>
+            </div>
+        </div>
+    </div>
+    <div class="ui vertical stripe segment">
+        <div class="ui middle aligned stackable grid container">
+            <div class="row">
+                <div class="eight wide column">
+                    <h3 class="ui header">
+                        We Help Companies and Companions
+                    </h3>
+                    <p>
+                        We can give your company superpowers to do things that they never thought possible. Let us delight your customers and empower your needs...through pure data analytics.
+                    </p>
+                    <h3 class="ui header">
+                        We Make Bananas That Can Dance
+                    </h3>
+                    <p>
+                        Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.
+                    </p>
+                </div>
+                <div class="six wide right floated column">
+                    <img class="ui large bordered rounded image" src="static/images/templates/semantic-ui/wireframe/white-image.png" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="center aligned column">
+                    <a class="ui huge button">Check Them Out</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ui vertical stripe quote segment">
+        <div class="ui equal width stackable internally celled grid">
+            <div class="center aligned row">
+                <div class="column">
+                    <h3>
+                        "What a Company"
+                    </h3>
+                    <p>
+                        That is what they all say about us
+                    </p>
+                </div>
+                <div class="column">
+                    <h3>
+                        "I shouldn't have gone with their competitor."
+                    </h3>
+                    <p>
+                        <img class="ui avatar image" src="static/images/templates/semantic-ui/avatar/nan.jpg" /><b>Nan</b>Chief Fun Officer Acme Toys
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ui vertical stripe segment">
+        <div class="ui text container">
+            <h3 class="ui header">
+                Breaking The Grid, Grabs Your Attention
+            </h3>
+            <p>
+                Instead of focusing on content creation and hard work, we have learned how to master the art of doing nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic and worth your attention.
+            </p>
+            <a class="ui large button">Read More</a>
+            <h4 class="ui horizontal header divider">
+                <a href="homepage.html#"> Case Studies</a>
+            </h4>
+            <h3 class="ui header">
+                Did We Tell You About Our Bananas?
+            </h3>
+            <p>
+                Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but its really true. It took years of gene splicing and combinatory DNA research, but our bananas can really dance.
+            </p>
+            <a class="ui large button">I'm Still Quite Interested</a>
+        </div>
+    </div>
+    <div class="ui inverted vertical footer segment">
+        <div class="ui container">
+            <div class="ui stackable inverted divided equal height stackable grid">
+                <div class="three wide column">
+                    <h4 class="ui inverted header">
+                        About
+                    </h4>
+                    <div class="ui inverted link list">
+                        <a class="item" href="homepage.html#"> Sitemap</a><a class="item" href="homepage.html#"> Contact Us</a><a class="item" href="homepage.html#"> Religious Ceremonies</a><a class="item" href="homepage.html#"> Gazebo Plans</a>
+                    </div>
+                </div>
+                <div class="three wide column">
+                    <h4 class="ui inverted header">
+                        Services
+                    </h4>
+                    <div class="ui inverted link list">
+                        <a class="item" href="homepage.html#"> Banana Pre-Order</a><a class="item" href="homepage.html#"> DNA FAQ</a><a class="item" href="homepage.html#"> How To Access</a><a class="item" href="homepage.html#"> Favorite X-Men</a>
+                    </div>
+                </div>
+                <div class="seven wide column">
+                    <h4 class="ui inverted header">
+                        Footer Header
+                    </h4>
+                    <p>
+                        Extra space for a call to action inside the footer that could help re-engage users.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include'inc/footer.php' ?>
+
