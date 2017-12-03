43e48d5c43c62dea7b81cef99fdba111e184dda7
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2017-12-03 13:18:12 +0100

    add favicon link

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <link rel="icon" href="assets/icones/favicon.ico">
   <meta charset="utf-8" />
   <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
   <meta content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=no" name="viewport" />
@@ -8,6 +9,7 @@
   <meta content="Replace" name="keywords" />
   <meta content="TechDeals team" name="author" />
   <meta content="#ffffff" name="theme-color" />
+
   <title>TechDeals</title>
   <link href="assets/static/dist/semantic-ui/semantic.min.css" rel="stylesheet" type="text/css" />
   <link href="assets/static/stylesheets/default.css" rel="stylesheet" type="text/css" />
