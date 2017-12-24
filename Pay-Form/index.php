e753ab678ba3f5e3b352f6bef75953be59d182ed
Maxence COLMANT <maxence.colmant@ynov.com>
2017-12-24 03:26:08 +0100

    Modif_v0.1_pay

--- Pay-Form/index.php
+++ Pay-Form/index.php
@@ -1,10 +1,6 @@
-<?php include 'includes/header.php' ?>
+<?php include "includes/header.php"; ?>
 
-<?php include 'includes/navbar.php' ?>
-
-
-
-
+<?php include "includes/navbar.php"; ?>
 
 
 <style>
