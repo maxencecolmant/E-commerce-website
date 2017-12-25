e98404395d2eab73bf679aa067cca2217bc3eadc
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2017-12-25 14:48:58 +0100

    add presentation page with about-us.php

---
+++ about-us.php
@@ -0,0 +1,160 @@
+<?php
+include_once "includes/init.php";
+include_once "includes/Session.php";
+include_once "includes/Util.php";
+include_once "includes/User.php";
+include 'includes/header.php'; 
+include 'includes/navbar.php'; 
+?>
+<section class="header-bckg">
+	<div class="container">
+		<div class="col-md-8 col-md-offset-2">
+			<h2 class="hr-title">TechDeals</h2>
+			<p class="description">Apple pie donut biscuit. Lollipop jujubes cookie. Gummi bears bonbon pastry gingerbread tart toffee bear claw bear claw muffin</p>
+			<div class="cta-links">
+				<a class="btn-like-link-standard" href="#presentation">En savoir plus</a>
+				<a class="buy" href="">Acheter</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="presentation">
+	<div class="container uses">
+		<h2>Pourquoi nous choisir ?</h2>
+		<div class="row">
+			<div class="col-md-4 use">
+				<h4>Livraison rapide</h4>
+				<img src="assets/images/fast.svg" alt="" width="90px">
+				<p>Powder cheesecake fruitcake danish jelly-o donut tart sweet. Fruitcake pie lemon drops pie.</p>
+			</div>
+			<div class="col-md-4 use">
+				<h4>Service client</h4>
+				<img src="assets/images/online.svg" alt=""  width="90px">
+				<p>Powder cheesecake fruitcake danish jelly-o donut tart sweet. Fruitcake pie lemon drops pie.</p>
+			</div>
+			<div class="col-md-4 use">
+				<h4>Paiement sécurisé</h4>
+				<img src="assets/images/secured.svg" alt=""  width="90px">
+				<p>Powder cheesecake fruitcake danish jelly-o donut tart sweet. Fruitcake pie lemon drops pie.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="clients">
+	<div class="container">
+		<h2>Avis clients</h2>
+	</div>
+</section>
+
+<section class="team">
+	<div class="container">
+		<h2>L'équipe</h2>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="main-content">
+					<div class="avatar">
+						<img src="/assets/images/laurent.jpg" width="140px" alt="">
+					</div>
+					<div class="text-content">
+						<div class="name">
+							Laurent Panek
+						</div>
+						<div class="job">
+							Développeur PHP </br> Sécurité Web
+						</div>
+						<div class="bio">
+							Bonbon topping cookie jelly-o icing brownie candy halvah bonbon. Carrot cake biscuit tiramisu cheesecake fruitcake wafer.
+						</div>
+						<div class="social">
+							<ul class="s-links">
+								<li>
+									<a href=""><i class="socicon-twitter"></i></a>
+								</li>
+								<li>
+									<a href=""><i class="socicon-linkedin"></i></a>
+								</li>
+								<li>
+									<a href=""><i class="socicon-github"></i></a>
+								</li>
+							</ul>
+							
+							
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="main-content">
+					<div class="avatar">
+						<img src="/assets/images/abdessalam.jpg" width="140px" alt="">
+					</div>
+					<div class="text-content">
+						<div class="name">
+							Abdessalam Benharira
+						</div>
+						<div class="job">
+							Développeur PHP </br> Web Designer
+						</div>
+						<div class="bio">
+							Bonbon topping cookie jelly-o icing brownie candy halvah bonbon. Carrot cake biscuit tiramisu cheesecake fruitcake wafer.
+						</div>
+						<div class="social">
+							<ul class="s-links">
+								<li>
+									<a href=""><i class="socicon-twitter"></i></a>
+								</li>
+								<li>
+									<a href=""><i class="socicon-linkedin"></i></a>
+								</li>
+								<li>
+									<a href=""><i class="socicon-github"></i></a>
+								</li>
+							</ul>
+							
+						</div>
+						
+					</div>
+				</div>	
+			</div>
+			<div class="col-md-4">
+				<div class="main-content">
+					<div class="avatar">
+						<img src="/assets/images/maxence.jpg" width="140px" alt="">
+					</div>
+					<div class="text-content">
+						<div class="name">
+							Maxence Colmant
+						</div>
+						<div class="job">
+							Développeur PHP </br> Chef de projet
+						</div>
+						<div class="bio">
+							Bonbon topping cookie jelly-o icing brownie candy halvah bonbon. Carrot cake biscuit tiramisu cheesecake fruitcake wafer.
+						</div>
+						<div class="social">
+							<ul class="s-links">
+								<li>
+									<a href=""><i class="socicon-twitter"></i></a>
+								</li>
+								<li>
+									<a href=""><i class="socicon-linkedin"></i></a>
+								</li>
+								<li>
+									<a href=""><i class="socicon-github"></i></a>
+								</li>
+							</ul>
+							
+							
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section> 
+
+<?php include 'includes/footer.php'; ?>
