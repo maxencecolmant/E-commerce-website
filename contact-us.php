301c3bf15d9f804bd3456d3ea6da25521c1664d6
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2018-01-06 17:56:31 +0100

    add js verif to contact form

--- contact-us.php
+++ contact-us.php
@@ -1,9 +1,40 @@
 <?php
-include 'includes/header.php'; 
-include 'includes/navbar.php'; 
+include 'includes/header.php';
+include 'includes/navbar.php';
 
 
-//add php for contact mail
+function isEmail($mail)
+{
+	return filter_var($mail, FILTER_VALIDATE_EMAIL);
+}
+
+if (!empty($_POST)) {
+	if (!empty($_POST['email']) && isEmail($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['message'])) {
+
+		function get_ip()
+		{
+			if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+				return $_SERVER['HTTP_CLIENT_IP'];
+			} elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+				return $_SERVER['HTTP_X_FORWARDED_FOR'];
+			} else {
+				return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
+			}
+		}
+		function get_user()
+		{
+			if (isset($_SERVER['HTTP_USER_AGENT'])) {
+				return $_SERVER['HTTP_USER_AGENT'];
+			}
+		}
+		$email = htmlspecialchars($_POST['email']);
+		$message = htmlspecialchars("Adresse IP: " . get_ip() . "\r\nUser-Agent: " . get_user() . "\r\nEmail: " . $email . "\r \n" . $_POST['message'] . "\r\n");
+		$to = "contact@techdeals.com";
+		$headers = "From: <$email>" . "\r\n";
+		$subject = "Contact TechDeals :" . $_POST['subject'];
+		mail($to, $subject, $message, $headers);
+	}
+}
 ?>
 <div class="container form">
 	<div class="col-md-6 col-md-offset-3 form-custom">
@@ -16,22 +47,23 @@ include 'includes/navbar.php';
 				</div>
 			</h2>
 			<div class="contact-form">
-				<form class="form" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
-					<div class="input-group">
-						<i class="socicon-mail custom-icon"></i>
-						<input id="contact-email" type="text" class="contact-email form-control" placeholder="Email" name="email">
+				<form class="form" id="mainForm" method="POST">
+					<div class="form-group">
+						<input id="email" type="email" class="contact-email form-control" placeholder="Email" name="email" required="">
+						<label id="email-error" class="error" for="email"></label>
 					</div>
-					<div class="input-group">
-						<i class="socicon-folder custom-icon"></i>
-						<input id="contact-subject" type="text" class="contact-subject form-control" placeholder="Sujet" name="subject">
+					<div class="form-group">
+						<input id="subject" type="text" class="contact-subject form-control" placeholder="Sujet" name="subject" required="">
+						<label id="subject-error" class="error" for="subject"></label>
                     </div>
                     
                     <div class="form-group">
-                        <textarea id="contact-message" class="contact-message form-control none" rows="8" placeholder="Message" name="message"></textarea>
+						<textarea id="message" class="contact-message form-control none" rows="8" placeholder="Message" name="message" required=""></textarea>
+						<label id="message-error" class="error" for="message"></label>
                     </div>
 	
 					<div class="text-center submit-button">
-						<input type="submit" class="btn-custom bttn-jelly bttn-md" value="Envoyer"/>
+						<input id="submit" type="submit" class="btn-custom bttn-jelly bttn-md" value="Envoyer"/>
 					</div>
 				</form>
 			</div>
