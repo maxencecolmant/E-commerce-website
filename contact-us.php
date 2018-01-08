525d08812c6bedd69ad4d78f732f771a59a8a81f
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2018-01-08 16:57:38 +0100

    :grinning: Oooops add button

--- contact-us.php
+++ contact-us.php
@@ -4,7 +4,6 @@ include 'includes/navbar.php';
 
 if (!empty($_POST)) {
     if (!empty($_POST['email']) && Util::isEmail($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['message'])) {
-
         $email = htmlspecialchars($_POST['email']);
         $message = htmlspecialchars("Adresse IP: " . Util::get_ip() . "\r\nUser-Agent: " . Util::get_user() . "\r\nEmail: " . $email . "\r \n" . $_POST['message'] . "\r\n");
         $to = "contact@techdeals.com";
@@ -14,38 +13,38 @@ if (!empty($_POST)) {
     }
 }
 ?>
-    <div class="container form">
-        <div class="col-md-6 col-md-offset-3 form-custom">
-            <div class="">
-                <h2 class="img-header">
-                    <img class="img-logo" src="assets/icones/logo.png"/>
-                    <div class="content">
-                        Formulaire de contact
+<div class="container form">
+    <div class="col-md-6 col-md-offset-3 form-custom">
+		<div class="">
+			<h2 class="img-header">
+				<img class="img-logo" src="assets/icones/logo.png"/>
+				<div class="content">
+					Formulaire de contact
+				</div>
+			</h2>
+			<div class="contact-form">
+				<form class="form" id="mainForm" method="POST" name="contactForm">
+					<div id="forEmail" class="form-group">
+						<input id="email" type="email" class="contact-email form-control" placeholder="Email" name="email" >
+						<label id="email-error" class="error" for="email"></label>
+					</div>
+					<div id="forSubject" class="form-group">
+						<input id="subject" type="text" class="contact-subject form-control" placeholder="Sujet" name="subject">
+						<label id="subject-error" class="error" for="subject"></label>
                     </div>
-                </h2>
-                <div class="contact-form">
-                    <form class="form" id="mainForm" method="POST" name="contactForm">
-                        <div id="forEmail" class="form-group">
-                            <input id="email" type="email" class="contact-email form-control" placeholder="Email"
-                                   name="email">
-                            <label id="email-error" class="error" for="email"></label>
-                        </div>
-                        <div id="forSubject" class="form-group">
-                            <input id="subject" type="text" class="contact-subject form-control" placeholder="Sujet"
-                                   name="subject">
-                            <label id="subject-error" class="error" for="subject"></label>
-                        </div>
+                    
+                    <div id="forMessage" class="form-group">
+						<textarea id="message" class="contact-message form-control none" rows="8" placeholder="Message" name="message"></textarea>
+						<label id="message-error" class="error" for="message"></label>
+                    </div>
 
-                        <div id="forMessage" class="form-group">
-                            <textarea id="message" class="contact-message form-control none" rows="8"
-                                      placeholder="Message" name="message"></textarea>
-                            <label id="message-error" class="error" for="message"></label>
-
-                        </div>
-                    </form>
-                </div>
+					<div class="text-center submit-button">
+ 						<input id="submit" type="submit" class="btn-custom bttn-jelly bttn-md" value="Envoyer"/>
+ 					</div>
+                </form>
             </div>
         </div>
     </div>
-    </div>
+</div>
+</div>
 <?php include 'includes/footer.php'; ?>
