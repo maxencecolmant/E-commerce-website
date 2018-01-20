395a4d96643431b6b0d0fe0bc705314990a6d2b1
Maxence COLMANT <maxence.colmant@ynov.com>
2018-01-20 21:30:46 +0100

    edition_profil

---
+++ user-profile.php
@@ -0,0 +1,99 @@
+<?php include 'includes/header.php'; ?>
+<?php $user->login(); ?>
+<?php include 'includes/navbar.php' ?>
+<style>
+    
+</style>
+
+<div class="container" style="text-align:center;">
+    <h1 class="page-header">Modifiez votre profil</h1>
+    <div class="row">
+        <!-- edit form column -->
+        <div class="">
+            <form class="form-horizontal" role="form">
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Civilitée:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" value="" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Prénom:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" value="" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Nom:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" value="" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Pays:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" value="" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Email:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control"  type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-3 control-label">Mot de passe:</label>
+                    <div class="col-md-8">
+                        <input class="form-control" value="11111122333" type="password">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-3 control-label">Confirmez votre mot de passe:</label>
+                    <div class="col-md-8">
+                        <input class="form-control" value="" type="password">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-3 control-label">Adresse:</label>
+                    <div class="col-md-8">
+                        <input class="form-control" value="" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-3 control-label">Complement d'adresse:</label>
+                    <div class="col-md-8">
+                        <input class="form-control" value="" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-3 control-label">Ville:</label>
+                    <div class="col-md-8">
+                        <input class="form-control" value="" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-3 control-label">Code postal:</label>
+                    <div class="col-md-8">
+                        <input class="form-control" value="" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-3 control-label">Téléphone:</label>
+                    <div class="col-md-8">
+                        <input class="form-control" value="" type="text">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-3 control-label"></label>
+                    <div class="col-md-8">
+                        <input class="btn btn-primary" value="Save Changes" type="button">
+                        <span></span>
+                        <input class="btn btn-default" value="Cancel" type="reset">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include 'includes/footer.php' ?>
